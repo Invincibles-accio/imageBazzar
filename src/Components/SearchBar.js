cfb660f1327f37c89f7a5937ee97593a4b9f27a3
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-14 22:27:18 +0530

    imageBazzar

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react"; 
+import axios from "axios";
+
+const SearchBar = ({addImages}) => {
+    let [searchTerm, setSearchTerm] = useState("");
+
+    // api => cat => list of cat images => data
+    const handleSearch = async (e) =>{
+        console.log("Code is hitting here")
+        e.preventDefault();
+        const response =  await axios.get("https://api.unsplash.com/search/photos",{
+               params: {query: searchTerm},
+               headers: {
+                Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+               }
+        })
+        addImages(response.data.results);
+       
+    }
+
+    return(
+        <div>
+              <input  
+                 type="text"
+                  placeholder="Search"
+                  onChange={(e)=>setSearchTerm(e.target.value)}
+                  value={searchTerm}
+              
+              />
+              <button type="button" onClick={handleSearch}>Search</button>
+            
+        </div>
+    )
+}
+
+export default SearchBar;
